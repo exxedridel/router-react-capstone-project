386fd1cf51559e41c94743c89c2051228e7f29e8
hevedrios <exxed.ridel@gmail.com>
2022-10-10 14:57:52 -0500

    Added: Display icons when image hovered

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Image = ({className, img}) => {
+function Image({ className, img }) {
+  const [hovered, setHovered] = useState(false);
+
+  const heartIcon = hovered && <i className="ri-heart-line favorite"></i>;
+  const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+
   return (
-    <div className={`${className} image-container` }>
-      <img className="image-grid" src={img.url} alt="" />
+    <div
+      className={`${className} image-container`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <img src={img.url} className="image-grid" />
+      {heartIcon}
+      {cartIcon}
     </div>
   );
-};
+}
 
 export default Image;
