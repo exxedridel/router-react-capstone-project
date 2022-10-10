22dfd94e872138a317056ffd478ef1f1ee32350a
hevedrios <exxed.ridel@gmail.com>
2022-10-10 13:30:01 -0500

    Added: Context Setup and his State

---
+++ src/AppContext.js
@@ -0,0 +1,11 @@
+import React, { createContext, useState } from "react";
+
+const Context = createContext();
+
+function ContextProvider({ children }) {
+  const [allPhotos, setAllPhotos] = useState([]);
+
+  return <Context.Provider value={{allPhotos}}>{children}</Context.Provider>;
+}
+
+export { ContextProvider, Context };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
+import { ContextProvider } from "./AppContext";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <StrictMode>
+    <ContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </ContextProvider>
+  </StrictMode>
 );
