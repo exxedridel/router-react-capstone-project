d35f9f5556b50abb4dc679a1840b55ae2d0c30a6
hevedrios <exxed.ridel@gmail.com>
2022-10-10 15:33:58 -0500

    Added: Display favorite icon when pressed

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,26 +1,41 @@
-import React, {useState, useContext} from "react"
-import {Context} from "../AppContext"
+import React, { useState, useContext } from "react";
+import { Context } from "../AppContext";
 
-function Image({className, img}) {
-    const [hovered, setHovered] = useState(false)
-    const {toggleFavorite} = useContext(Context)
-    
-    const heartIcon = hovered && 
-        <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
-        
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
-    
-    return (
-        <div 
-            className={`${className} image-container`}
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-        >
-            <img src={img.url} className="image-grid"/>
-            {heartIcon}
-            {cartIcon}
-        </div>
-    )
+function Image({ className, img }) {
+  const [hovered, setHovered] = useState(false);
+  const { toggleFavorite } = useContext(Context);
+
+  function heartIcon() {
+    if (img.isFavorite) {
+      return (
+        <i
+          className="ri-heart-fill favorite"
+          onClick={() => toggleFavorite(img.id)}
+        ></i>
+      );
+    } else if (hovered) {
+      return (
+        <i
+          className="ri-heart-line favorite"
+          onClick={() => toggleFavorite(img.id)}
+        ></i>
+      );
+    }
+  }
+
+  const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+
+  return (
+    <div
+      className={`${className} image-container`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <img src={img.url} className="image-grid" />
+      {heartIcon()}
+      {cartIcon}
+    </div>
+  );
 }
 
-export default Image
+export default Image;
