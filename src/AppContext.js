80178bd007d61d5ef8882639854b94a41422f512
hevedrios <exxed.ridel@gmail.com>
2022-10-10 14:34:15 -0500

    Added: Fetch and map photos

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,9 +1,15 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 const Context = createContext();
 
 function ContextProvider({ children }) {
   const [allPhotos, setAllPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+    .then(res => res.json())
+    .then(data => setAllPhotos(data))
+  }, [])
 
   return <Context.Provider value={{allPhotos}}>{children}</Context.Provider>;
 }
