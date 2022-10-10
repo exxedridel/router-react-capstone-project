d3d9dfdb2f9e75c8a5b5e224478d8daeb5618d62
hevedrios <exxed.ridel@gmail.com>
2022-10-10 15:32:27 -0500

    Added: Toggle is Favorite

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,17 +1,35 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, {useState, useEffect} from "react"
 
-const Context = createContext();
+const Context = React.createContext()
 
-function ContextProvider({ children }) {
-  const [allPhotos, setAllPhotos] = useState([]);
-
-  useEffect(() => {
-    fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
-    .then(res => res.json())
-    .then(data => setAllPhotos(data))
-  }, [])
-
-  return <Context.Provider value={{allPhotos}}>{children}</Context.Provider>;
+function ContextProvider({children}) {
+    const [allPhotos, setAllPhotos] = useState([])
+    
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+    
+    function toggleFavorite(id) {
+        const updatedArr = allPhotos.map(photo => {
+            if(photo.id === id) {
+                console.log(id)
+                console.log(!photo.isFavorite)
+                return {...photo, isFavorite: !photo.isFavorite}
+            }
+            return photo
+        })
+        
+        setAllPhotos(updatedArr)
+    }
+    
+    return (
+        <Context.Provider value={{allPhotos, toggleFavorite}}>
+            {children}
+        </Context.Provider>
+    )
 }
 
-export { ContextProvider, Context };
+export {ContextProvider, Context}
