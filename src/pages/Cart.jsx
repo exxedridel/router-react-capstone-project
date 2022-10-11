5ea5eebf91cd643ef10f00bb9205ee2aaa28265c
hevedrios <exxed.ridel@gmail.com>
2022-10-10 19:57:53 -0500

    Added: Calculate total cost

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,21 +3,24 @@ import {Context} from "../AppContext"
 import CartItem from "../components/CartItem"
 
 function Cart() {
-    const {cartItems} = useContext(Context)
-    const cartItemElements = cartItems.map(item => (
-        <CartItem key={item.id} item={item} />
-    ))
-    
-    return (
-        <main className="cart-page">
-            <h1>Check out</h1>
-            {cartItemElements}
-            <p className="total-cost">Total: </p>
-            <div className="order-button">
-                <button>Place Order</button>
-            </div>
-        </main>
-    )
+   const {cartItems} = useContext(Context)
+   const totalCost = 5.99 * cartItems.length
+   const totalCostDisplay = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
+   
+   const cartItemElements = cartItems.map(item => (
+       <CartItem key={item.id} item={item} />
+   ))
+   
+   return (
+       <main className="cart-page">
+           <h1>Check out</h1>
+           {cartItemElements}
+           <p className="total-cost">Total: {totalCostDisplay}</p>
+           <div className="order-button">
+               <button>Place Order</button>
+           </div>
+       </main>
+   )
 }
 
 export default Cart
